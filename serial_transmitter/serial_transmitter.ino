1ff4a2be1743a055b22d978aaf127e16a8a8c6d9
Alfred Broda <AlfredBroda@gmail.com>
2015-10-30 18:21:06 +0100

    Useful serial transmission sketch

---
+++ serial_transmitter/serial_transmitter.ino
@@ -0,0 +1,36 @@
+/**
+ * SerialReflector
+ */
+// #include <SoftwareSerial.h>
+
+// SoftwareSerial device(2,3); // Rx, Tx
+
+int LED = 17;
+
+void setup() {
+  Serial.begin(115200);   // Serial port for connection to host
+  while(!Serial) {
+    delay(10);
+  }
+  Serial.println("Serial on-line!");
+  //  device.begin(9600);  // Serial port for connection to serial device
+  Serial1.begin(9600);
+  while(!Serial1) {
+    delay(10);
+  }
+  Serial.println("Serial1 on-line!");
+}
+
+void loop() {
+  if(Serial1.available()) {
+    Serial.write(Serial1.read());
+  }
+
+  if(Serial.available()) {
+    char mesg = Serial.read();
+    Serial.write(mesg);
+    Serial1.write(mesg);
+  }
+}
+
+
