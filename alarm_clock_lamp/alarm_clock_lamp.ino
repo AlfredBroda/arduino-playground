ced95b8f871d7aa2ce08c19ff8c04e37ca00e2e0
Alfred <AlfredBroda@gmail.com>
2016-10-16 20:40:45 +0100

    SevenSegmentTM1637, lib info
    Use SevenSegmentTM1637 for display module, more info on libraries in header

--- alarm_clock_lamp/alarm_clock_lamp.ino
+++ alarm_clock_lamp/alarm_clock_lamp.ino
@@ -1,30 +1,39 @@
 /*
-  DS3231: Real-Time Clock (with alarm)
-  Based on: www.jarzebski.pl/arduino/komponenty/zegar-czasu-rzeczywistego-rtc-DS3231.html
+  DS3231 + TM1637 Real-Time Clock (with temperature display and alarm)
+  Based on http://www.jarzebski.pl/arduino/komponenty/zegar-czasu-rzeczywistego-rtc-ds3231.html
+  Libraries used:
+  SevenSegmentTM1637 https://github.com/bremme/arduino-tm1637
+  DS3231 https://github.com/jarzebski/Arduino-DS3231
   (c) 2016 by Alfred Broda
 */
 
 #include <Wire.h>
 #include <DS3231.h>
-#include <TM1637Display.h>
+#include <SevenSegmentTM1637.h>
+#include <SevenSegmentExtended.h>
 
+// RTC clock uses pins 2 DIO and 3 for CLK as default for Wire library
 DS3231 clock;
 RTCDateTime dt;
 
-// Module connection pins (Digital Pins)
-#define CLK 5
-#define DIO 4
-TM1637Display clockDisplay(CLK, DIO);
-bool clockDot = false;
+// Display module connection pins (Digital Pins)
+#define DISP_CLK 5
+#define DISP_DIO 4
 
-#define LAMP 6
+SevenSegmentExtended display(DISP_CLK, DISP_DIO);
+
+// For switching displayed information
+bool displayTemp = false;
+
+// Alarm digital pin for starting the alarm function
+#define ALARM 6
 
 void setup()
 {
-  Serial.begin(9600);
-  
+//  Serial.begin(9600);
+
   // Initialize DS3231
-  Serial.println("Initialize DS3231");;
+//  Serial.println("Initialize DS3231");;
   clock.begin();
 
   // Disarm alarms and clear alarms for this example, because alarms is battery backed.
@@ -33,16 +42,18 @@ void setup()
   clock.armAlarm2(false);
   clock.clearAlarm1();
   clock.clearAlarm2();
- 
-  // Set Alarm - Every 05h:30m:00s in each day
+
+  // Set Alarm - Every 05h:30m:00s each day
   // setAlarm1(Date or Day, Hour, Minute, Second, Mode, Armed = true)
   clock.setAlarm1(0, 5, 30, 00, DS3231_MATCH_H_M_S);
 
   // Initialize display
-  clockDisplay.setBrightness(0x08);
-  
-  pinMode(LAMP, OUTPUT);
-  digitalWrite(LAMP, LOW);
+  display.off();
+  display.setBacklight(50);
+  display.on();
+
+  pinMode(ALARM, OUTPUT);
+  digitalWrite(ALARM, LOW);
 }
 
 void loop()
@@ -50,36 +61,42 @@ void loop()
   dt = clock.getDateTime();
 
   Serial.println(clock.dateFormat("d-m-Y H:i:s - l", dt));
-  
-  clockDisplay.showNumberDec((dt.hour * 100) + dt.minute);
-  if(clockDot) 
+
+  if (displayTemp)
   {
-//    clockDisplay.point(1);
     clock.forceConversion();
 
     Serial.print("Temperature: ");
     int temp = clock.readTemperature();
-    Serial.println(temp);
-    clockDisplay.showNumberDec(temp);
+    Serial.print(temp);
+    Serial.println(" deg C");
+
+    uint8_t seg_temp[] = {
+      display.encode(temp / 10),
+      display.encode(temp % 10),
+      TM1637_CHAR_STAR,        
+      TM1637_CHAR_C
+    };
+    display.printRaw(seg_temp);
   } else {
-//    clockDisplay.point(0);
+    display.setColonOn(true);
+    display.printTime(dt.hour, dt.minute, true);
   }
-  clockDot = !clockDot;
+  displayTemp = !displayTemp;
 
   // Call isAlarm1(false) if you want clear alarm1 flag manualy by clearAlarm1();
   if (clock.isAlarm1())
   {
     Serial.println("ALARM 1 TRIGGERED!");
-    digitalWrite(LAMP, HIGH);
+    digitalWrite(ALARM, HIGH);
   }
 
   // Call isAlarm2(false) if you want clear alarm1 flag manualy by clearAlarm2();
   if (clock.isAlarm2())
   {
     Serial.println("ALARM 2 TRIGGERED!");
-    digitalWrite(LAMP, HIGH);
+    digitalWrite(ALARM, HIGH);
   }
- 
+
   delay(1000);
 }
-
