71a695f6af714f4e4965b9ea2de86d0c352646e7
Alfred Broda <alfred.broda@allegro.pl>
2018-11-18 20:52:25 +0100

    Improved power management

--- sensored_watering/sensored_watering.ino
+++ sensored_watering/sensored_watering.ino
@@ -8,42 +8,52 @@
 const int sensPin = 6; // pin number of the sensor
 const int outPin = 4; // pump switch pin
 
-const int homeSwitchPower = 2; // power for home/endstop switch
-const int homeSwitchPin = 3; // pin for checking if home position is reached
+const int homePin = 3; // pin for checking if home position is reached
 
-const bool CWise = true;
+const bool CWise = false;
 
-CheapStepper stepper (15,14,16,10);
+CheapStepper stepper(15,14,16,10);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
   pinMode(outPin, OUTPUT);
   pinMode(sensPin, INPUT);
+  pinMode(homePin, INPUT);
 
-  pinMode(homeSwitchPower, OUTPUT);
-  digitalWrite(homeSwitchPower, HIGH);
-
-  pinMode(homeSwitchPin, INPUT);
-
-  stepper.setRpm(10);
+  stepper.setRpm(15);
   home();
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  checkSensor(sensPin, 90);
-  delay(100);
+  checkSensor(sensPin, 30);
+//  checkSensor(sensPin, 90);
+//  checkSensor(sensPin, 150);
+//  checkSensor(sensPin, 210);
+//  checkSensor(sensPin, 270);
+  delay(1000);
 }
 
 void home() {
-  while(digitalRead(homeSwitchPin) == LOW) {
+  while(digitalRead(homePin) == LOW) {
     stepper.stepCCW();
   }
   stepper.stop();
-  while(digitalRead(homeSwitchPin) == HIGH) {
+  
+  while(digitalRead(homePin) == HIGH) {
     stepper.stepCW();
   }  
   stepper.stop();
+  
+}
+
+void powerDown() {
+  digitalWrite(15, LOW);
+  digitalWrite(14, LOW);
+  digitalWrite(16, LOW);
+  digitalWrite(10, LOW);
 }
 
 void checkSensor(int pin, int angle) {
@@ -55,6 +65,7 @@ void checkSensor(int pin, int angle) {
     }
     digitalWrite(outPin, LOW);    // turn the pump output off
     home(); // reset valve
+    powerDown();
   }
 }
 
