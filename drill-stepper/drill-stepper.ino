a48b5456bc26ed3d308a0aa118b5d37565533a7e
Alfred <alfred.broda@gmail.com>
2019-09-19 23:33:45 +0200

    Update drill-stepper.ino

--- drill-stepper/drill-stepper.ino
+++ drill-stepper/drill-stepper.ino
@@ -1,24 +1,28 @@
 #include <TM1637Display.h>
 
 #define BAUD (9600)
-#define DEFDELAY 100
-#define ENABLE 8     // all axis enable pin
-#define MMSTEPSZ 320 // 320 steps is 1mm by default on TMC chips
+#define DEBUG true
+
+#define DEFDELAY (100)
+#define MMSTEPSZ (320) // 320 steps is 1mm by default on TMC chips
+#define MAXZ (300)     // 30cm is the maximum z axis displacement
 
 // Z axis stepper driver pins
+#define ENABLE 8 // shared by all axis
 #define STEPZ 7
 #define DIRZ 4
 #define ENDSTOPZ 11
 #define POSZ A6
 
-int x; 
 int step_delay_z;
 bool current_dir; // forward or reverse
 bool error;
 
 bool run_z;
-int pos_z;
-int dest_z;
+long pos_z;
+long dest_z;
+int dest_z_mm;
+int disp_timer;
 
 TM1637Display display(A5, A4);
 
@@ -49,22 +53,25 @@ void setup()
   display.setBrightness(0x01);
 
   pinMode(ENABLE, OUTPUT); // Enable
-  pinMode(STEPZ, OUTPUT); // Step
-  pinMode(DIRZ, OUTPUT); // Dir
+  pinMode(STEPZ, OUTPUT);  // Step
+  pinMode(DIRZ, OUTPUT);   // Dir
+  pinMode(POSZ, INPUT);    // Position knob
   pinMode(ENDSTOPZ, INPUT_PULLUP); // Endstop default closed
 
-  error = 0;
-  pos_z = 0;
   step_delay_z = DEFDELAY;
-  current_dir = true;
-  x = 0;
+  current_dir = false; // set reverse 
+  disp_timer = 0;
 
-  homeZAxis();
+  error = true; // we don't know the position of the machine
+  pos_z = 0L;    // should actually be undefined
+  dest_z = 0L;
+  run_z = true; // enable z axis
 }
 
 void loop() 
 {
   if (!error) {
+    serialPrint("Start running main loop");
     // Loop
     while(run_z)
     {
@@ -77,21 +84,62 @@ void loop()
       // get destination
       readPosSetZ();
       // check where we are
-      if (pos_z < dest_z) {
+      if (pos_z == dest_z) {
+        serialPrint("At set position:");
+        serialPrint(dest_z);
+        for (int x = 0; x < 6; x++) {
+          if (x%2 == 0) {
+            display.setBrightness(0x0f);
+          } else {
+            display.setBrightness(0x01);
+          }
+          display.showNumberDec(dest_z_mm, false);
+          delay(500);
+        }
+      } else if (dest_z > pos_z) {
         doStepZ(true);
-      } else if (pos_z > dest_z) {
+      } else if (dest_z < pos_z) {
         doStepZ(false);
       }
       updateCounter();
     }
+    if (!run_z) {
+      readPosSetZ();
+      updateCounter();
+      delay(100);
+    }
+  } else {
+      homeZAxis();
+      run_z = true;
   }
 }
 
-void updateCounter() {
-    display.showNumberDec((pos_z*10)/(MMSTEPSZ), false);
+int readPosSetZ() {
+  int posz_read = analogRead(POSZ);
+  dest_z_mm = round(posz_read*MAXZ/1024);
+  dest_z = round(dest_z_mm*MMSTEPSZ);
 }
-void doStepZ(const bool dir) 
-{
+
+void updateCounter() {
+  if (disp_timer%MMSTEPSZ == 0) {
+    if (disp_timer < 10*MMSTEPSZ) {
+      display.setBrightness(0x0f);
+      display.showNumberDec(dest_z_mm, false);
+    } else {  
+      display.setBrightness(0x01);
+      display.showNumberDec(round(pos_z/MMSTEPSZ), false);
+    }
+    serialPrint("Position/destination");
+    serialPrint(pos_z);
+    serialPrint(dest_z);
+  }
+  disp_timer += 1;
+  if (disp_timer > 20*MMSTEPSZ) {
+    disp_timer = 0;
+  }
+}
+
+void doStepZ(const bool dir) {
     current_dir = dir;
     digitalWrite(DIRZ, dir); // Set Dir
     digitalWrite(STEPZ, HIGH); // Output high
@@ -118,14 +166,26 @@ void homeZAxis() {
   }
   // back off
   reverseDir();
-  while (digitalRead(ENDSTOPZ) == HIGH) {
+  for (int x = 0; x < MMSTEPSZ; x++) {
     doStepZ(current_dir);
   }
-  display.setBrightness(0x0f);
+
   serialPrint("Homing Z axis done.");
-  display.setSegments(SEG_DONE);
-  delay(2000);
+  for (int x = 0; x < 6; x++) {
+    if (x%2 == 0) {
+      display.setBrightness(0x0f);
+    } else {
+      display.setBrightness(0x01);
+    }
+    display.setSegments(SEG_DONE);
+    delay(500);
+  }
   display.setBrightness(0x01);
+  
+  display.clear();
+
+  pos_z = 0;
+  error = false;
 }
 
 void reverseDir() {
@@ -142,26 +202,34 @@ bool checkEndstopZ() {
       serialPrint("Z axis endstop reached!");
       display.setBrightness(0x0f);
       display.setSegments(SEG_ERRZ); 
+      
+      // reverse and back off
       reverseDir();
-      while (digitalRead(ENDSTOPZ) == HIGH) {
+      for (int x = 0; x < MMSTEPSZ; x++) {
         doStepZ(current_dir);
       }
       serialPrint("Z axis endstop cleared.");
+      
       // disable steppers, wait for reset
       digitalWrite(ENABLE, HIGH);
-      error = 1;
+      error = true;
       return true;
   }
-  return 0;
-}
-
-int readPosSetZ() {
-  int dest_mm = analogRead(A6);
-  dest_z = dest_mm * MMSTEPSZ;
+  return false;
 }
 
 void serialPrint(const String &text) {
-    Serial.print(millis());
-    Serial.print(": ");
-    Serial.println(text);
+    if (DEBUG){
+      Serial.print(millis());
+      Serial.print(": ");
+      Serial.println(text);
+    }
+}
+
+void serialPrint(const int text) {
+    if (DEBUG){
+      Serial.print(millis());
+      Serial.print(": ");
+      Serial.println(text);
+    }
 }
